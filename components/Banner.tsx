b9193589cfa3e117f638da788a77b8ddee9b9ffe
Mischa Saunders <mischa.saunders@gmail.com>
2022-01-29 13:55:37 +1300

    Update banner to show registration live

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,7 +4,8 @@ export default function Banner() {
   return (
     <Box bg="wiha.400" fontWeight="bold" w="100%" p={5}>
       <Center fontSize="xl" textAlign="center">
-        Registration for 2022 opens soon!
+        Registration for 2022 now open!&nbsp;
+        <a href="/news/2022-registration">Click here to read more </a>
       </Center>
     </Box>
   );
